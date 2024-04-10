cce27e38d740700cf432e29cfbe018065ef5605a
b0gd4n2001 <lulelarub@gmail.com>
2024-04-10 07:37:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/", function (req, res) {
+  res.json({unix: Date.now(), utc: new Date().toUTCString()});
+})
+
 app.get("/api/:date", function (req, res) {
   if (Number.isNaN(Number(req.params.date))) {
     if (new Date(req.params.date) == 'Invalid Date') {
